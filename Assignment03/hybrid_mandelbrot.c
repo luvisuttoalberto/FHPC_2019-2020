37f4609a1cc71c327c5cb39df606a39c51b20882
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2020-01-13 20:16:30 +0100

    assignment

--- Assignment03/hybrid_mandelbrot.c
+++ Assignment03/hybrid_mandelbrot.c
@@ -42,6 +42,86 @@ int write_pgm_image(int maxval, int xsize, int ysize, const char *image_name ){
 }
 
 
+
+int get_cpu_id( void )
+{
+#if defined(_GNU_SOURCE)                              // GNU SOURCE ------------
+  
+  return  sched_getcpu( );
+
+#else
+
+#ifdef SYS_getcpu                                     //     direct sys call ---
+  
+  int cpuid;
+  if ( syscall( SYS_getcpu, &cpuid, NULL, NULL ) == -1 )
+    return -1;
+  else
+    return cpuid;
+  
+#else      
+
+  unsigned val;
+  if ( read_proc__self_stat( CPU_ID_ENTRY_IN_PROCSTAT, &val ) == -1 )
+    return -1;
+
+  return (int)val;
+
+#endif                                                // -----------------------
+#endif
+
+}
+
+
+
+int read_proc__self_stat( int field, int *ret_val )
+/*
+  Other interesting fields:
+
+  pid      : 0
+  father   : 1
+  utime    : 13
+  cutime   : 14
+  nthreads : 18
+  rss      : 22
+  cpuid    : 39
+
+  read man /proc page for fully detailed infos
+ */
+{
+  // not used, just mnemonic
+  // char *table[ 52 ] = { [0]="pid", [1]="father", [13]="utime", [14]="cutime", [18]="nthreads", [22]="rss", [38]="cpuid"};
+
+  *ret_val = 0;
+
+  FILE *file = fopen( "/proc/self/stat", "r" );
+  if (file == NULL )
+    return -1;
+
+  char   *line = NULL;
+  int     ret;
+  size_t  len;
+  ret = getline( &line, &len, file );
+  fclose(file);
+
+  if( ret == -1 )
+    return -1;
+
+  char *savetoken = line;
+  char *token = strtok_r( line, " ", &savetoken);
+  --field;
+  do { token = strtok_r( NULL, " ", &savetoken); field--; } while( field );
+
+  *ret_val = atoi(token);
+
+  free(line);
+
+  return 0;
+}
+
+
+
+
 int main(int argc, char ** argv){
 
 	//struct timespec ts;
@@ -154,6 +234,18 @@ int main(int argc, char ** argv){
 				local_buffer = (unsigned char *) realloc(local_buffer, len * sizeof(unsigned char));
 			}
 			stop = work_index[0] + work_index[1];
+			#pragma omp parallel{
+				int me = omp_get_thread_num();
+				#pragma omp master{
+					n_threads = omp_get_num_threads();
+					printf("omp prefix sum with %d threads \n", nthreads);
+				}
+				#pragma omp critical
+				{
+					printf("thread %2d is running on core %2d\n", me, get_cpu_id() );
+				}
+			}
+
 
 			#pragma omp parallel for firstprivate(delta_x, delta_y, x_L, y_L, I_max, n_x, n_y) collapse(2) schedule(guided)
 			for(int i = work_index[0]; i < stop; ++i){
